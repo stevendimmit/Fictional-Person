2b538936e42b04a523b1c59e8b4e97094eafe608
daviddimmit <daviddimmit@localhost>
2024-04-20 15:17:13 -0600

    First Commit

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+
+public class Person {
+
+	public static void main(String[] args){
+		
+		// Here we declare our variables as strings
+		// and set them to their values
+		
+		String firstName = "Tom";
+		String lastName = "Adams";
+		String streetAddress = "15 Main Ave";
+		String city = "Ocean City";
+		String zipCode = "12345";
+		
+		//Here we print our output with descriptions
+		//of each variable
+		
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+
+	}
+
+}
